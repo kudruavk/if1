84ea38d54cef917249d6b5619a7573e735c9a0cb
kudruavk <92382034+kudruavk@users.noreply.github.com>
2022-02-04 17:03:52 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+ /* let a=prompt('4islo')
+if (a>5){
+    alert('chislo > 5')
+}
+else {
+    alert('chislo <5')
+} */
+// let a='2' > '12'
+// alert(a)
+            // let age=+prompt('')
+            // if ((14<=age) && (age<=90)) {
+            //     alert('DA')
+            // }
+            // else {
+            //     alert('NET')
+            // }
+// let a= prompt('one')
+// let b= prompt('dva')
+// let res = (a + b < 4) ? 'mnogo' : 'malo';
+// alert(res)
+        let a=prompt('one')
+        let b=prompt('dva')
+        let c=prompt('tri')
+        if  ((a>b) && (b>c)) {
+            alert(a+','+b+','+c)
+        }
+        else{
+            if ((b > c) && (c>a)){
+               
+                    alert(b+','+c+','+a)
+                }
+                else{
+                    alert(c+','+a+','+b)
+                    if((a<c) && (a<b)){
+                        alert(c + b + a)
+                    }
+                }
+            }
+      
+           
+        
